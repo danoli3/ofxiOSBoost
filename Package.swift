e6bc6f3af8a7444743a3aa125c305604944062a1
Dan Rosser <danoli3@gmail.com>
2026-09-02 18:57:10 +1000

    Match Boost 1.86.0 published checksum

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .binaryTarget(
             name: "boost",
             url: "https://github.com/danoli3/ofxiOSBoost/releases/download/1.86.0/ofxiOSBoost-1.86.0-xcframework.zip",
-            checksum: "3c0b58b7a4add380f025e97bceb6f0e08932790f474e482f281dccca2841d039"
+            checksum: "1bdf528117c426999b17143789152de1f7c29f0f72bc146e2cad2c1abdd02d41"
         ),
         .target(
             name: "ofxiOSBoost",
