e69eeb74bbea32e321cbc0108284cd43e4c7520a
Dan Rosser <danoli3@gmail.com>
2026-09-03 15:25:38 +1000

    Finalize Boost 1.87.0 artifact checksum

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .binaryTarget(
             name: "boost",
             url: "https://github.com/danoli3/ofxiOSBoost/releases/download/1.87.0/ofxiOSBoost-1.87.0-xcframework.zip",
-            checksum: "0132ea36d9b82bba5625e74d3a146ea2eb7576a8c79d32844839461e7b798d14"
+            checksum: "515804becc1952656eec0aa49ae13b41fd4087ae28bb694a2aca3f2eee77ee44"
         ),
         .target(
             name: "ofxiOSBoost",
